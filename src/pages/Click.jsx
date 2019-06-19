3087e29b55e44a260aa5586b2d79117727cd9af1
Gisli Nielsen <gisli@innit.no>
2019-06-19 11:31:30 +0200

    Made Click into a funcitonal component

--- src/pages/Click.jsx
+++ src/pages/Click.jsx
@@ -1,37 +1,19 @@
 import React from 'react';
 import '../App.css';
 
-class Click extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			count: 0,
-		};
-	};
-
-	handleCount = (value) => {
-		if (value === 'reset') {
-			this.setState({ count: 0 });
-		} else {
-			this.setState({ count: value });
-		}
-		document.title = value;
-	};
-
-	render() {
+const Click = () => {
 		return (
 			<div className="container">
 				<div className="vertical-center horizontal-center flex-1 column">
-					<h3>Count: {this.state.count}</h3>
+					<h3>Count:</h3>
 					<div>
-						<button className="primary-bg" onClick={() => this.handleCount(this.state.count + 1)}>Add</button>
-						<button className="secondary-bg" onClick={() => this.handleCount('reset')}>Reset</button>
-						<button className="danger-bg" onClick={() => this.handleCount(this.state.count - 1)}>Subtract</button>
+						<button className="primary-bg">Add</button>
+						<button className="secondary-bg">Reset</button>
+						<button className="danger-bg">Subtract</button>
 					</div>
 				</div>
 			</div>
-		);
-	}
+	);
 };
 
 export default Click;
